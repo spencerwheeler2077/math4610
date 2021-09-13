fd4a4432d93c9eb47714abfb102826788e37c73b
svenwheeler <spencer.wheeler@aggiemail.usu.edu>
2021-09-13 15:56:02 -0600

    Just pushing a new file for homework purposes.

---
+++ SecDerivative.py
@@ -0,0 +1,22 @@
+import numpy
+
+h = [1]
+for j in range(1, 17):
+    h.append(h[-1]/10)
+
+x = 2
+exact = -numpy.cos(x)
+error = []
+
+for i in h:
+
+    approxDer = (numpy.cos(x+i)-2*numpy.cos(x)+numpy.cos(x-i))/(i**2)
+    error.append(abs(exact-approxDer))
+
+
+print("h-value   error")
+for i in range(len(h)):
+    if i < 10:
+        print(f"1e-{i}      {error[i]}")
+    else:
+        print(f"1e-{i}     {error[i]}")
